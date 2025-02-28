57afd9ea65dad0c73fa382044254c4cf63622898
leandro-gois <leandroag2007@gmail.com>
2025-02-28 02:23:28 -0300

    Primeiros Passos em Spring Boot

---
+++ Spring Framework/primeiros-passos/src/main/java/com/lgois/springboot/PrimeirosPassosApplication.java
@@ -0,0 +1,18 @@
+package com.lgois.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+	}
+}
