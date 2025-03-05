b399f646bacd5bf10026afd002535a903c6f5eb6
leandro-gois <leandroag2007@gmail.com>
2025-03-05 17:44:40 -0300

    Beans Versus Components

---
+++ Spring Framework/primeiros-passos/src/main/java/com/lgois/springboot/BeansversusComponents/Beans.java
@@ -0,0 +1,13 @@
+package com.lgois.springboot.BeansversusComponents;
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Beans {
+    @Bean
+    public Gson gson(){
+        return new Gson();
+    }
+}
