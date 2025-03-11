0a3a3f9ee322ae32741384617766e526b2edfbba
leandro-gois <leandroag2007@gmail.com>
2025-03-11 00:13:57 -0300

    Scope: Singleton X Prototype

--- Spring Framework/singleton-ou-prototype/src/main/java/lgois/springboot/SingletonOuPrototypeApplication.java
+++ Spring Framework/singleton-ou-prototype/src/main/java/lgois/springboot/SingletonOuPrototypeApplication.java
@@ -1,13 +1,23 @@
 package lgois.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SingletonOuPrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SingletonOuPrototypeApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(SistemaMensagem sistemaMensagem){
+		return args -> {
+			sistemaMensagem.enviarConfirmacaoCadastro();
+			sistemaMensagem.enviarMensagemBoasVindas();
+			sistemaMensagem.enviarConfirmacaoCadastro();
+		};
 	}
 
 }
