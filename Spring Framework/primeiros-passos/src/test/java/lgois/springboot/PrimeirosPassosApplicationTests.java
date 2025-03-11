8f8c9e42724dd7bbd34cfc9f431bc3651c7f93f0
leandro-gois <leandroag2007@gmail.com>
2025-03-10 21:04:30 -0300

    Primeiros Passos com o Spring Boot

---
+++ Spring Framework/primeiros-passos/src/test/java/lgois/springboot/PrimeirosPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package lgois.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
