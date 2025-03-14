706bc5551d02a2aa403d9c2fb73ea359365597f6
leandro-gois <leandroag2007@gmail.com>
2025-03-14 20:34:08 -0300

    Spring Web API -> REST API e SWAGGER

---
+++ Spring Framework/spring-web-api/src/test/java/lgois/api/SpringWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lgois.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
